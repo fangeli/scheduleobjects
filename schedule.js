44d76cfe0458f10c4eb5f0c1b984ce6f28b62b80
Fabio Angeli <fangeli@divitech.it>
2019-03-15 16:14:21 +0100

    release 1.0.0

---
+++ schedule.js
@@ -0,0 +1,109 @@
+﻿
+class schedule { 
+
+    constructor() {
+
+        this._id = null;
+        this._idReport = null;
+        this._json = null;
+        this._startDate = null;
+        this._path = null;
+        this._state = null;
+        this._da = null;
+        this._a = null;
+        this._timestamp = null;
+        this._idUser = null;
+        this._idCompany = null;
+    }
+
+    getId() {
+        return this._id;
+    }
+
+    getIdReport() {
+        return this._idReport;
+    }
+
+    getJson() {
+        return this._json;
+    }
+
+    getStartDate() {
+        return this._startDate;
+    }
+
+    getPath() {
+        return this._path;
+    }
+
+    getState() {
+        return this._state;
+    }
+    getDa() {
+        return this._da;
+    }
+
+    getA() {
+        return this._a;
+    }
+
+    getTimestamp() {
+        return this._timestamp;
+    }
+
+    getIdUser() {
+        return this._idUser;
+    }
+
+    getIdCompany() {
+        return this._idCompany;
+    }
+
+    setId(value) {
+        this._id = value;
+    }
+
+    setIdReport(value) {
+        this._idReport = value;
+    }
+
+    setJson(value) {
+        this._json = value;
+    }
+
+    setStartDate(value) {
+        this._startDate = value;
+    }
+
+    setPath(value) {
+        this._path = value;
+    }
+
+    setState(value) {
+        this._state = value;
+    }
+
+    setDa(value) {
+        this._da = value;
+    }
+
+    setA(value) {
+        this._a = value;
+    }
+
+    setTimestamp(value) {
+        this._timestamp = value;
+    }
+
+    setIdUser(value) {
+        this._idUser = value;
+    }
+     
+    setIdCompany(value) {
+        this._idCompany = value;
+    }
+}
+
+
+
+module.exports = schedule
